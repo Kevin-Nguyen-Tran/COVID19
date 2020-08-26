35bd7697a23390491142d5c59517e96dd10d3105
Kevin Tran <k.n.tran898@gmail.com>
2020-08-26 14:33:02 -0400

    Commiting the reorganization of the data analysis and adding page breaks with comments explaining what is covered in the next section. Will be useful when creating TOC for ReadMe.

--- Covid19_US_RScript.R
+++ Covid19_US_RScript.R
@@ -12,6 +12,10 @@ data <- as_tibble(covid19_data) #changes data set into a tibble (used in conjunc
 data <- data %>%
   mutate(date = ymd(date)) #fixed formatting with dates to YYYY-MM-DD
 
+#Data is now in the correct format. Now we can filter as necessary to target data sets of interest for exploration. -----------------------------------------------
+# Below is the data on Florida, we will look at positive cases and trends, proportion of cases that are either positive or negative, visualize the difference
+# in positive and negative cases in Florida, and death trends in Florida.
+
 #desired data for only Florida
 FL_data <- data %>%
   filter(state == "FL") %>%
@@ -72,20 +76,7 @@ deaths <- ggplot(data = FL_data, mapping = aes(x = date, y = death)) +
   theme(plot.title = element_text(hjust = 0.5),
         plot.subtitle = element_text(hjust = 0.5))
 
-# Look at total number of deaths per state (from inception to 2020-08-07) Will give us an idea of populations to compare.
-death_cases <- data %>%
-  filter(date == "2020-08-07") %>%
-  arrange(desc(death)) %>%
-  select(date : state, death)
-
-# We will also look at total number of cases (positive and negative) within California, Texas, and Florida
-states_of_interest <- c("FL", "CA", "TX")
-
-total_cases <- data %>%
-  filter(date == "2020-08-07", state %in% states_of_interest) %>%
-  select(date : negative) %>%
-  mutate(total_cases = positive + negative) %>%
-  arrange(total_cases)
+#------------------------------------------------------------------------------------------------------------------------
 
 # We will also look at total number of cases (positive and negative) of all states
 total_cases_all <- data %>%
@@ -93,17 +84,50 @@ total_cases_all <- data %>%
   select(date : negative) %>%
   mutate(total_testings = positive + negative) %>% 
   arrange(desc(positive))
-# As shown in the above data set, California has more positive cases than Florida, and Florida has more positive cases than Texas.
+
+# Look at total number of deaths per state (from inception to 2020-08-07) Will give us an idea of populations to compare.
+death_cases <- data %>%
+  filter(date == "2020-08-07") %>%
+  arrange(desc(death)) %>%
+  select(date : state, death)
+
+# We will also look at total number of cases (positive and negative) within Florida, California, and Texas.
+states_of_interest <- c("FL", "CA", "TX")
+
+total_cases <- data %>%
+  filter(date == "2020-08-07", state %in% states_of_interest) %>%
+  select(date : negative) %>%
+  mutate(total_cases = positive + negative) %>%
+  arrange(total_cases)
+# As shown in the above data set: total_cases_all, California has more positive cases than Florida, and Florida has more positive cases than Texas. 
+
 # CA, FL, and TX are the top 3 states with the most positive cases in the U.S. We will compare Florida to these states to analyze two factors: death and recovery.
+FL_TX_CA_data <- data %>%
+  filter(state == "FL" | state == "TX" | state == "CA") %>%
+  arrange(state, date) %>%
+  select(date : state, death)
+
+ggplot(data = FL_TX_CA_data, mapping = aes(x = date, y = death, color = state)) +
+  geom_point() +
+  geom_smooth() +
+  labs(x = "Date", 
+       y = "Deaths",
+       title = "Deaths Over Time in California, Florida, and Texas",
+       subtitle = "March 2020 - August 2020",
+       caption = "Source: https://covidtracking.com/") +
+  theme(plot.title = element_text(hjust = 0.5),
+        plot.subtitle = element_text(hjust = 0.5))
+
+# Add recovery plot similar to above graph [HERE] ***
 
 #----------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 # We will first start by comparing the average difference in deaths between Florida and California.
-# Conducting a Independent 2-sample t-test and CI to see if Florida and California's average deaths differ 
+# Conducting a Independent 2-sample t-test and generating CI to see if Florida and California's average deaths differ 
 # as the states with the highest amounts of cases as of August 2020.
-# This t-test will tell us how significant the difference of death rates are between Florida and California
-# Null Hypothesis (Ho): The death rate of Florida and California are the same (do not differ and equal to zero)
-# Alternative Hypothesis (Ha): The death rate of Florida and California are different (not equal to zero)
+# This t-test will tell us how significant the average difference of deaths are between Florida and California
+# Null Hypothesis (Ho): The average difference of death for Florida and California are the same (do not differ and equal to zero)
+# Alternative Hypothesis (Ha): The average difference of death for Florida and California are different (not equal to zero)
 
 #To test the hypothesis, we will use the t.test(). However, we will need the data set to show data for only FL and CA
 FL_CA_data <- data %>%
@@ -113,7 +137,15 @@ FL_CA_data <- data %>%
 
 #Visualize the variance between the two states with a box plot! As we can see, CA has more variability than FL over the same time frame
 ggplot(data = FL_CA_data) +
-  geom_boxplot(aes(x = state, y = death)) 
+  geom_boxplot(aes(x = state, y = death)) +
+  coord_flip() +
+  labs(x = "Deaths", 
+       y = "State",
+       title = "Deaths in Florida vs California",
+       subtitle = "March 2020 - August 2020",
+       caption = "Source: https://covidtracking.com/") +
+  theme(plot.title = element_text(hjust = 0.5),
+        plot.subtitle = element_text(hjust = 0.5))
 
 #Now we can run the t.test keeping in mind, our null hypothesis is equal to zero, alternative is a two.sided, 95% confidence interval and variance is not equal and they are independent populations
 t.test(FL_CA_data$death ~ FL_CA_data$state, mu = 0, alt = "two.sided", conf = 0.95, var.eq = FALSE, paired = FALSE)
@@ -136,6 +168,11 @@ average_deaths_per_day_CA <- mean(death_CA$diff_death, na.rm = TRUE)
 # Average deaths per day in California is 67.61
 
 #Now we can repeat for FL:
+FL_TX_data <- data %>%
+  filter(state == "FL" | state == "TX") %>%
+  arrange(state, date) %>%
+  select(date : state, death)
+
 death_FL <- FL_TX_data %>%
   filter(state == "FL") %>%
   mutate(diff_day = date - lag(date),
@@ -148,11 +185,14 @@ average_death_rate_FL <- mean(death_FL$rate_percent, na.rm = TRUE)
 average_deaths_per_day_FL <- mean(death_FL$diff_death, na.rm = TRUE)
 # Average deaths per day in Florida is 54.02
 
-# We will conduct the same analysis as above, but comparing the mean difference between Florida and Texas.
+#--------------------------------------------------------------------------------------------------------------------------------
+
+# We will conduct the same analysis as above, but comparing the average difference in deaths between Florida and Texas.
 # Keep in mind, that Florida has more positive cases than Texas as shown above in the data set total_cases.
-# Null Hypothesis (Ho): The death rate of Florida and Texas are the same (do not differ and equal to zero)
-# Alternative Hypothesis (Ha): The death rate of Florida and Texas are different (not equal to zero)
+# Null Hypothesis (Ho): The average difference of death for Florida and Texas are the same (do not differ and equal to zero)
+# Alternative Hypothesis (Ha): The The average difference of death for Florida and Texas are different (not equal to zero)
 # To test the hypothesis, we will use the t.test(). However, we will need the data set to show data for only FL and TX
+
 FL_TX_data <- data %>%
   filter(state == "FL" | state == "TX") %>%
   arrange(state, date) %>%
@@ -176,37 +216,12 @@ t.test(FL_TX_data$death ~ FL_TX_data$state, mu = 0, alt = "two.sided", conf = 0.
 # With 95% confidence, we can say that the difference within means fall between 98.05 to 1014.56
 # Since zero is not in the confidence interval as well, we can reject the null hypothesis with 95% confidence.
 
-# Analyzing the TX box plot with the variance in number of deaths, it shows that there was a huge spike of deaths
+# Analyzing the box plot with the variance in number of deaths for Texas, it shows that there was a huge spike of deaths
 # by the outliers shown between 6,000 to 8,000 deaths. Based on that, could we hypothesize that the rate of death
 # is greater in Texas than in Florida?
 
 #Ho: The rate of death in Texas is the same as that of Florida
 #Ha: The rate of death in Texas in greater than that of Florida
-
-#Visualize the variance between the two states with a box plot! As we can see, FL has more variability then TX over the same time frame
-ggplot(data = FL_TX_data) +
-  geom_boxplot(aes(x = state, y = death)) +
-  coord_flip() +
-  labs(x = "Deaths", 
-       y = "State",
-       title = "Deaths in Florida vs Texas",
-       subtitle = "March 2020 - August 2020",
-       caption = "Source: https://covidtracking.com/") +
-  theme(plot.title = element_text(hjust = 0.5),
-        plot.subtitle = element_text(hjust = 0.5))
-
-# Fitting a non-linear regression for the relationship between Y = death, X1 = date, and X2 = state (TX and FL)
-# This is only showing the total numbers of death over 5 months, not the rate at which death occurs. We will calculate this rate below!
-ggplot(data = FL_TX_data, mapping = aes(x = date, y = death, color = state)) +
-  geom_point() +
-  geom_smooth() +
-  labs(x = "Date", 
-       y = "Deaths",
-       title = "Deaths Over Time in Florida vs Texas",
-       subtitle = "March 2020 - August 2020",
-       caption = "Source: https://covidtracking.com/") +
-  theme(plot.title = element_text(hjust = 0.5),
-        plot.subtitle = element_text(hjust = 0.5))
 
 # Calculating the average growth rate per day for each respective state 
 death_TX <- FL_TX_data %>%
@@ -215,61 +230,48 @@ death_TX <- FL_TX_data %>%
          diff_death = death - lag(death),
          rate_percent = diff_death/lag(death) * 100)
   
-# Now we can calculate the average rate of growth:
+# Now we can calculate the average rate of growth in Texas:
 average_death_rate_TX <- mean(death_TX$rate_percent, na.rm = TRUE)
 # Average rate of death is 7.14% per day in TX.
+average_deaths_per_day_TX <- mean(death_TX$diff_death, na.rm = TRUE)
+# Average deaths per day in Texas is 56.61
 
-# Now we can repeat for FL:
-death_FL <- FL_TX_data %>%
-  filter(state == "FL") %>%
-  mutate(diff_day = date - lag(date),
-         diff_death = death - lag(death),
-         rate_percent = diff_death/lag(death) * 100) #Final Value - Starting Value / Starting Value * 100
-
-# Now we can calculate the average rate of growth:
+# Note, from the previous hypothesis test between FL and CA. Florida's average death rate and average deaths per day are as shown below:
 average_death_rate_FL <- mean(death_FL$rate_percent, na.rm = TRUE)
-# Average rate of death is 6.13% per day in FL.
+#Average rate of death is 6.13% per day in FL.
+average_deaths_per_day_FL <- mean(death_FL$diff_death, na.rm = TRUE)
+# Average deaths per day in Florida is 54.02
 
-# Average rate of death over a 5 month period between March 2020 and August 2020, TX showed the greatest death rate
-# To visualize, we can create a scatter plot and add a line of best fit as shown below
+tribble(
+  ~state, ~avg_percent_inc_death, ~avg_deaths_per_day,
+  "CA", average_death_rate_CA, average_deaths_per_day_CA,
+  "FL", average_death_rate_FL, average_deaths_per_day_FL,
+  "TX", average_death_rate_TX, average_deaths_per_day_TX
+)
+# It is interesting to see that despite have the most cases, California has the lowest average percent increase in deaths out of the three states.
+# It is also interesting to see that Texas, with the lowest amount of positive cases, has the highest average percent increase in deaths.
 
-death_FL_TX_CA <- rbind(death_FL, death_TX, death_CA) # Combining the two data frames together to visualize death rates 
+
+# Average rate of death over a 5 month period between March 2020 and August 2020, TX showed the greatest rate of death
+# To visualize, we will fit a non-linear regression for the relationship between Y = death rate, X1 = date, and X2 = state (TX, FL and CA)
+# This is will show the rate of death over 5 months, between TX, FL, and CA.
+death_FL_TX_CA <- rbind(death_FL, death_TX, death_CA) # Combining the three data frames together to visualize death rates 
 
 ggplot(data = death_FL_TX_CA, mapping = aes(x = date, y = diff_death, color = state)) +
   geom_point() +
   geom_smooth(se = FALSE) + #added line of best fit to show death rates per state
   labs(x = "Date", 
-       y = "Rate of Deaths per Day",
+       y = "% Increase of Deaths per Day",
        title = "Rapid Growth of Texas Death Rate",
        subtitle = "March 2020 - August 2020",
        caption = "Source: https://covidtracking.com/") +
   theme(plot.title = element_text(hjust = 0.5),
         plot.subtitle = element_text(hjust = 0.5))
-# Based on death rates, we can conclude that Texas has the highest at 7.14% per day, Florida with 6.13% per day, and California with the lowest at 5.81% per day
-
-
-
+# Based on percent increase of deaths per day, we can conclude visually and mathematically that Texas has the highest at 7.14% increase per day, Florida with 6.13% increase per day, and California with the lowest at 5.81% increase per day
+# We will now look at the recovery among the three states!
 
 #-------------------------------------------------------------------------------------------------------------------------------
-#IF NEEDED AT THE END ***
 
-
-death_FL_CA <- rbind(death_FL, death_CA) #combining the two data frames together to visualize death rates 
-
-ggplot(data = death_FL_CA, mapping = aes(x = date, y = diff_death, color = state)) +
-  geom_point() +
-  geom_smooth(se = FALSE) + #added line of best fit to show death rates per state
-  labs(x = "Date", 
-       y = "Deaths per Day",
-       title = "Rapid Growth of Florida Death Rate",
-       subtitle = "March 2020 - August 2020",
-       caption = "Source: https://covidtracking.com/") +
-  theme(plot.title = element_text(hjust = 0.5),
-        plot.subtitle = element_text(hjust = 0.5))
-
-# As shown in this scatter plot and average death rate calculated above, the rate of death in Florida is greater than that of California.
-# In mid July, we see the state of Florida takes an upswing in deaths to be far greater than that of California, despite California having more positive cases.
-# More research is needed to understand why this is the case. Shortly, we will look into the recovery rates.
 
 
 
